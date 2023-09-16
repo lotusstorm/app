a164d6a7ca9504150075c804967811b62d6abaed
Islam Utizhev <iutizev@gmail.com>
2023-09-17 02:27:14 +0300

    config

---
+++ app.vue
@@ -0,0 +1,15 @@
+<template>
+  <div ref="root" />
+</template>
+
+<script setup lang="ts">
+const root = ref();
+
+onMounted(() => {
+  // const app = usePixi({ width: 640, height: 360 });
+
+  // // console.log(app, '$pixi');
+  // root.value.appendChild(app.view);
+})
+
+</script>
---
+++ nuxt.config.ts
@@ -0,0 +1,11 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  devtools: { enabled: true },
+  plugins: [
+    // {
+    //   src: '~/plugins/',
+    //   ssr: false,
+    //   mode: 'client'
+    // }
+  ],
+})
