351fe71fd46711576c9c7ad2fb31a5a81d3efc94
Islam Utizhev <iutizev@gmail.com>
2023-09-26 01:17:28 +0300

    step-11

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   app: {
-    baseURL: '/nuxt-github-pages/', // baseURL: '/<repository>/'
+    baseURL: '/app/', // baseURL: '/<repository>/'
     buildAssetsDir: 'assets', // don't use "_" at the begining of the folder name to avoids nojkill conflict
   },
   devtools: { enabled: true },
