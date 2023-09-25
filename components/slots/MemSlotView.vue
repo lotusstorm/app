5b5b8c363a0cf2854e1dc48a54f2aa4f6aa77e0e
Islam Utizhev <iutizev@gmail.com>
2023-09-26 02:52:53 +0300

    step-14

--- components/slots/MemSlotView.vue
+++ components/slots/MemSlotView.vue
@@ -104,27 +104,27 @@ watch(imgWebm, () => {
 })
 
 const dict = {
-  '/tyrant.png': '1', // done
-  '/chonguk.jpg': '2', // need
-  '/julia_black.jpg': '3', // need
-  '/leon.png': '4', // done
-  '/naruto.png': '5', // done
-  '/pain.jpg': '6', // done
-  '/seven.png': '7', // done
-  '/one_pice_black.png': '8', // need
-  '/one_pice_netflix_zoro.jpg': '9', // done
-  '/B.jpg': 'b', // done
-  '/T.png': 't', // done
-  '/S.jpg': 's' // done
+  '/app/tyrant.png': '1', // done
+  '/app/chonguk.jpg': '2', // need
+  '/app/julia_black.jpg': '3', // need
+  '/app/leon.png': '4', // done
+  '/app/naruto.png': '5', // done
+  '/app/pain.jpg': '6', // done
+  '/app/seven.png': '7', // done
+  '/app/one_pice_black.png': '8', // need
+  '/app/one_pice_netflix_zoro.jpg': '9', // done
+  '/app/B.jpg': 'b', // done
+  '/app/T.png': 't', // done
+  '/app/S.jpg': 's' // done
 }
 
 const video = {
   111: {
-    video: '/video/papich_ninada_diadia.webm',
+    video: '/app/video/papich_ninada_diadia.webm',
     duration: 8000,
   }, // papich
   222: {
-    video: '/video/chonguk_press_2.webm',
+    video: '/app/video/chonguk_press_2.webm',
     duration: 5000,
   }, // chonguk
   333: {
@@ -132,108 +132,108 @@ const video = {
     duration: 8000,
   }, // juli
   444: {
-    video: '/video/leon_krinj.webm',
+    video: '/app/video/leon_krinj.webm',
     duration: 8000,
   }, // leon
   555: {
-    video: '/video/naruto_blue_bird.webm',
+    video: '/app/video/naruto_blue_bird.webm',
     duration: 8000,
   }, // naruto
   666: {
-    video: '/video/pain_kek.webm',
+    video: '/app/video/pain_kek.webm',
     duration: 8000,
   }, // pain
   777: {
-    video: '/video/papich_eto_mne.webm',
+    video: '/app/video/papich_eto_mne.webm',
     duration: 8000,
   }, // papich
   888: {
-    video: '/video/one_pice.webm',
+    video: '/app/video/one_pice.webm',
     duration: 8000,
   }, // one
   999: {
-    video: '/video/one_pice_netflix_zoro.webm',
+    video: '/app/video/one_pice_netflix_zoro.webm',
     duration: 8000,
   }, // zoro
   1000: {
-    video: '/video/sanboy_tyajalo.webm',
+    video: '/app/video/sanboy_tyajalo.webm',
     duration: 8000,
   }, // sanboy
   bts: {
-    video: '/video/bts.webm',
+    video: '/app/video/bts.webm',
     duration: 5000,
   },
   // ======================================
   344: {
-    video: '/video/julia_leon_reaction.webm',
+    video: '/app/video/julia_leon_reaction.webm',
     duration: 8000,
   }, // juli + leon
   443: {
-    video: '/video/julia_leon_reaction.webm',
+    video: '/app/video/julia_leon_reaction.webm',
     duration: 8000,
   }, // juli + leon
   433: {
-    video: '/video/julia_leon_reaction.webm',
+    video: '/app/video/julia_leon_reaction.webm',
     duration: 8000,
   }, // juli + leon
   334: {
-    video: '/video/julia_leon_reaction.webm',
+    video: '/app/video/julia_leon_reaction.webm',
     duration: 8000,
   }, // juli + leon
   343: {
-    video: '/video/julia_leon_reaction.webm',
+    video: '/app/video/julia_leon_reaction.webm',
     duration: 8000,
   }, // juli + leon
   434: {
-    video: '/video/julia_leon_reaction.webm',
+    video: '/app/video/julia_leon_reaction.webm',
     duration: 8000,
   }, // juli + leon
   311: {
-    video: '/video/julia_tyrant_reaction.webm',
+    video: '/app/video/julia_tyrant_reaction.webm',
     duration: 8000,
   }, // juli + tyrant
   113: {
-    video: '/video/julia_tyrant_reaction.webm',
+    video: '/app/video/julia_tyrant_reaction.webm',
     duration: 8000,
   }, // juli + tyrant
   133: {
-    video: '/video/julia_tyrant_reaction.webm',
+    video: '/app/video/julia_tyrant_reaction.webm',
     duration: 8000,
   }, // juli + tyrant
   331: {
-    video: '/video/julia_tyrant_reaction.webm',
+    video: '/app/video/julia_tyrant_reaction.webm',
     duration: 8000,
   }, // juli + tyrant
   313: {
-    video: '/video/julia_tyrant_reaction.webm',
+    video: '/app/video/julia_tyrant_reaction.webm',
     duration: 8000,
   }, // juli + tyrant
   131: {
-    video: '/video/julia_tyrant_reaction.webm',
+    video: '/app/video/julia_tyrant_reaction.webm',
     duration: 8000,
   }, // juli + tyrant
   233: {
-    video: '/video/chonguk_press_1.webm',
+    video: '/app/video/chonguk_press_1.webm',
     duration: 4000,
   }, // chonguk + juli ??
   332: {
-    video: '/video/chonguk_press_1.webm',
+    video: '/app/video/chonguk_press_1.webm',
     duration: 4000,
   }, // chonguk + juli ??
   223: {
-    video: '/video/chonguk_press_1.webm',
+    video: '/app/video/chonguk_press_1.webm',
     duration: 4000,
   }, // chonguk + juli ??
   322: {
-    video: '/video/chonguk_press_1.webm',
+    video: '/app/video/chonguk_press_1.webm',
     duration: 4000,
   }, // chonguk + juli ??
   323: {
-    video: '/video/chonguk_press_1.webm',
+    video: '/app/video/chonguk_press_1.webm',
     duration: 4000,
   }, // chonguk + juli ??
   232: {
-    video: '/video/chonguk_press_1.webm',
+    video: '/app/video/chonguk_press_1.webm',
     duration: 8000,
   }, // chonguk + juli ??
   339: {
@@ -261,141 +261,147 @@ const video = {
     duration: 8000,
   }, // zoro + juli
   556: {
-    video: '/video/pain_naruto_2.webm',
+    video: '/app/video/pain_naruto_2.webm',
     duration: 8000,
   }, // naruto + pain
   665: {
-    video: '/video/pain_naruto_1.webm',
+    video: '/app/video/pain_naruto_1.webm',
     duration: 8000,
   }, // naruto + pain
   566: {
-    video: '/video/pain_naruto_2.webm',
+    video: '/app/video/pain_naruto_2.webm',
     duration: 8000,
   }, // naruto + pain
   655: {
-    video: '/video/pain_naruto_1.webm',
+    video: '/app/video/pain_naruto_1.webm',
     duration: 8000,
   }, // naruto + pain
   565: {
-    video: '/video/pain_naruto_1.webm',
+    video: '/app/video/pain_naruto_1.webm',
     duration: 8000,
   }, // naruto + pain
   656: {
-    video: '/video/pain_naruto_1.webm',
+    video: '/app/video/pain_naruto_1.webm',
     duration: 8000,
   }, // naruto + pain
   441: {
-    video: '/video/leon_vs_tyrant.webm',
+    video: '/app/video/leon_vs_tyrant.webm',
     duration: 8000,
   }, // leon + tyrant
   144: {
-    video: '/video/leon_vs_tyrant_3.webm',
+    video: '/app/video/leon_vs_tyrant_3.webm',
     duration: 8000,
   }, // leon + tyrant
   411: {
-    video: '/video/leon_vs_tyrant.webm',
+    video: '/app/video/leon_vs_tyrant.webm',
     duration: 8000,
   }, // leon + tyrant
   114: {
-    video: '/video/leon_vs_tyrant_3.webm',
+    video: '/app/video/leon_vs_tyrant_3.webm',
     duration: 8000,
   }, // leon + tyrant
   141: {
-    video: '/video/leon_vs_tyrant_3.webm',
+    video: '/app/video/leon_vs_tyrant_3.webm',
     duration: 8000,
   }, // leon + tyrant
   414: {
-    video: '/video/leon_vs_tyrant_3.webm',
+    video: '/app/video/leon_vs_tyrant_3.webm',
     duration: 8000,
   }, // leon + tyrant
   998: {
-    video: '/video/one_pice_zoro.webm',
+    video: '/app/video/one_pice_zoro.webm',
     duration: 8000,
   }, // zoro + one_pice
   889: {
-    video: '/video/one_pice_zoro.webm',
+    video: '/app/video/one_pice_zoro.webm',
     duration: 8000,
   }, // zoro + one_pice
   988: {
-    video: '/video/one_pice_zoro.webm',
+    video: '/app/video/one_pice_zoro.webm',
     duration: 8000,
   }, // zoro + one_pice
   899: {
-    video: '/video/one_pice_zoro.webm',
+    video: '/app/video/one_pice_zoro.webm',
     duration: 8000,
   }, // zoro + one_pice
   898: {
-    video: '/video/one_pice_zoro.webm',
+    video: '/app/video/one_pice_zoro.webm',
     duration: 8000,
   }, // zoro + one_pice
   989: {
-    video: '/video/one_pice_zoro.webm',
+    video: '/app/video/one_pice_zoro.webm',
     duration: 8000,
   }, // zoro + one_pice
   229: {
-    video: '/video/gachi_gym_boss.webm',
+    video: '/app/video/gachi_gym_boss.webm',
     duration: 5000,
   }, // zoro + chonguk
   992: {
-    video: '/video/gachi_gym_boss.webm',
+    video: '/app/video/gachi_gym_boss.webm',
     duration: 5000,
   }, // zoro + chonguk
   299: {
-    video: '/video/gachi_gym_boss.webm',
+    video: '/app/video/gachi_gym_boss.webm',
     duration: 5000,
   }, // zoro + chonguk
   922: {
-    video: '/video/gachi_gym_boss.webm',
+    video: '/app/video/gachi_gym_boss.webm',
     duration: 5000,
   }, // zoro + chonguk
   292: {
-    video: '/video/gachi_gym_boss.webm',
+    video: '/app/video/gachi_gym_boss.webm',
     duration: 5000,
   }, // zoro + chonguk
   929: {
-    video: '/video/gachi_gym_boss.webm',
+    video: '/app/video/gachi_gym_boss.webm',
     duration: 5000,
   }, // zoro + chonguk
   833: {
-    video: '/video/twitch_ban.webm',
+    video: '/app/video/twitch_ban.webm',
     duration: 8000,
   }, // juli + one_pice => ban
   338: {
-    video: '/video/twitch_ban.webm',
+    video: '/app/video/twitch_ban.webm',
     duration: 8000,
   }, // juli + one_pice => ban
   883: {
-    video: '/video/twitch_ban.webm',
+    video: '/app/video/twitch_ban.webm',
     duration: 8000,
   }, // juli + one_pice => ban
   388: {
-    video: '/video/twitch_ban.webm',
+    video: '/app/video/twitch_ban.webm',
     duration: 8000,
   }, // juli + one_pice => ban
   383: {
-    video: '/video/twitch_ban.webm',
+    video: '/app/video/twitch_ban.webm',
     duration: 8000,
   }, // juli + one_pice => ban
   838: {
-    video: '/video/twitch_ban.webm',
+    video: '/app/video/twitch_ban.webm',
     duration: 8000,
   }, // juli + one_pice => ban
 }
 
-Assets.load([
-  '/tyrant.png',
-  '/chonguk.jpg',
-  '/julia_black.jpg',
-  '/leon.png',
-  '/seven.png',
-  '/pain.jpg',
-  '/naruto.png',
-  '/one_pice_black.png',
-  '/one_pice_netflix_zoro.jpg',
+// const fff = useRuntimeConfig()
+
+// console.log(fff, 'fff');
+
+const assets = [
+  '/app/tyrant.png',
+  '/app/chonguk.jpg',
+  '/app/julia_black.jpg',
+  '/app/leon.png',
+  '/app/seven.png',
+  '/app/pain.jpg',
+  '/app/naruto.png',
+  '/app/one_pice_black.png',
+  '/app/one_pice_netflix_zoro.jpg',
   // '/B.jpg',
   // '/T.png',
   // '/S.jpg',
-]).then(onAssetsLoaded)
+]
+
+Assets.load(assets).then(onAssetsLoaded)
 
 const handleStart = () => {
   startPlay()
